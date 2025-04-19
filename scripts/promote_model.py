1dc154c64c6dc54caaafa038a4ae2133043fbd0d
AKSHAY SHARMA <akshay_02@outlook.com>
2025-04-19 15:23:42 +0530

    added promote model

--- scripts/promote_model.py
+++ scripts/promote_model.py
@@ -3,7 +3,7 @@ import mlflow
 
 def promote_model():
     # Set up AWS MLflow tracking URI
-    mlflow.set_tracking_uri("http://ec2-56-228-27-32.eu-north-1.compute.amazonaws.com:5000/")
+    mlflow.set_tracking_uri("http://ec2-13-60-211-129.eu-north-1.compute.amazonaws.com:5000/")
 
     client = mlflow.MlflowClient()
 
