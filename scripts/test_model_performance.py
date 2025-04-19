96949f06bf1eb753f8ac0864f1bdb12742e636db
AKSHAY SHARMA <akshay_02@outlook.com>
2025-04-19 19:04:21 +0530

    add flask api testing

--- scripts/test_model_performance.py
+++ scripts/test_model_performance.py
@@ -5,7 +5,7 @@ from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_sc
 import mlflow
 
 # Set your remote tracking URI
-mlflow.set_tracking_uri("http://ec2-13-60-211-129.eu-north-1.compute.amazonaws.com:5000/")
+mlflow.set_tracking_uri("http://ec2-13-49-240-199.eu-north-1.compute.amazonaws.com:5000/")
 
 @pytest.mark.parametrize("model_name, stage, holdout_data_path, vectorizer_path", [
     ("yt_chrome_plugin_model", "staging", "data/interim/test_processed.csv", "tfidf_vectorizer.pkl"),  # Replace with your actual paths
